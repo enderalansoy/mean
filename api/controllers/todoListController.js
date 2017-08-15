de235417db59a027e6c750bbde21f96520baa62d
enderalansoy <aendersoy@gmail.com>
2017-08-15 13:22:34 +0300

    Completed all routings.

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -102,6 +102,64 @@ exports.create_a_vehicle = function(req, res) {
     });
 };
 
+exports.read_a_vehicle = function(req, res) {
+    vehicle.findById(req.params.vehicleId, function(err, vehicle) {
+        if (err)
+            res.send(err);
+        res.json(vehicle);
+    });
+};
+
+exports.update_a_vehicle = function(req, res) {
+    vehicle.findOneAndUpdate({ _id: req.params.vehicleId },
+        req.body, { new: true },
+        function(err, vehicle) {
+            if (err)
+                res.send(err);
+            res.json(vehicle);
+        });
+};
+
+exports.delete_a_vehicle = function(req, res) {
+    vehicle.remove({
+        _id: req.params.vehicleId
+    }, function(err, vehicle) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'vehicle successfully deleted' });
+    });
+};
+
+exports.read_a_vehicle_from_vin = function(req, res) {
+    vehicle.findOne({
+        'vin': req.params.vehicleVin
+    }, function(err, vehicle) {
+        if (err)
+            res.send(err);
+        res.json(vehicle);
+    });
+};
+
+exports.update_a_vehicle_from_vin = function(req, res) {
+    vehicle.findOneAndUpdate({ 'vin': req.params.vehicleVin },
+        req.body, { new: true },
+        function(err, vehicle) {
+            if (err)
+                res.send(err);
+            res.json(vehicle);
+        });
+};
+
+exports.delete_a_vehicle_from_vin = function(req, res) {
+    vehicle.remove({
+        'tc': req.params.vehicleVin
+    }, function(err, vehicle) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'vehicle successfully deleted' });
+    });
+};
+
 // job controllers:
 
 exports.list_all_jobs = function(req, res) {
@@ -118,5 +176,80 @@ exports.create_a_job = function(req, res) {
         if (err)
             res.send(err);
         res.json(job);
+    });
+};
+
+exports.read_a_job = function(req, res) {
+    job.findById(req.params.jobId, function(err, job) {
+        if (err)
+            res.send(err);
+        res.json(job);
+    });
+};
+
+exports.update_a_job = function(req, res) {
+    job.findOneAndUpdate({ _id: req.params.jobId },
+        req.body, { new: true },
+        function(err, job) {
+            if (err)
+                res.send(err);
+            res.json(job);
+        });
+};
+
+exports.delete_a_job = function(req, res) {
+    job.remove({
+        _id: req.params.jobId
+    }, function(err, job) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'job successfully deleted' });
+    });
+};
+
+// usage controllers
+
+exports.list_all_usages = function(req, res) {
+    usage.find({}, function(err, usage) {
+        if (err)
+            res.send(err);
+        res.json(usage);
+    });
+};
+
+exports.create_a_usage = function(req, res) {
+    var new_usage = new usage(req.body);
+    new_usage.save(function(err, job) {
+        if (err)
+            res.send(err);
+        res.json(usage);
+    });
+};
+
+exports.read_a_usage = function(req, res) {
+    usage.findById(req.params.usageId, function(err, usage) {
+        if (err)
+            res.send(err);
+        res.json(usage);
+    });
+};
+
+exports.update_a_usage = function(req, res) {
+    usage.findOneAndUpdate({ _id: req.params.usageId },
+        req.body, { new: true },
+        function(err, usage) {
+            if (err)
+                res.send(err);
+            res.json(usage);
+        });
+};
+
+exports.delete_a_usage = function(req, res) {
+    usage.remove({
+        _id: req.params.usageId
+    }, function(err, usage) {
+        if (err)
+            res.send(err);
+        res.json({ message: 'usage successfully deleted' });
     });
 };
--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -2,6 +2,7 @@
 module.exports = function(app) {
     var todoList = require('../controllers/todoListController');
 
+    // user routes:
     app.route('/users')
         .get(todoList.list_all_users)
         .post(todoList.create_a_user);
@@ -16,15 +17,38 @@ module.exports = function(app) {
         .put(todoList.update_a_user_from_tc)
         .delete(todoList.delete_a_user_from_tc);
 
+    // vehicle routes:
     app.route('/vehicles')
         .get(todoList.list_all_vehicles)
         .post(todoList.create_a_vehicle);
 
+    app.route('/vehicles/id/:vehicleId')
+        .get(todoList.read_a_vehicle)
+        .put(todoList.update_a_vehicle)
+        .delete(todoList.delete_a_vehicle);
+
+    app.route('/vehicles/vin/:vehicleVin')
+        .get(todoList.read_a_vehicle_from_vin)
+        .put(todoList.update_a_vehicle_from_vin)
+        .delete(todoList.delete_a_vehicle_from_vin);
+
+    // job routes:
     app.route('/jobs')
         .get(todoList.list_all_jobs)
         .post(todoList.create_a_job);
 
-    /* app.route('usage')
-           .get(todoList.list_all_usages)
-           .post(todoList.create_a_usage); */
+    app.route('/jobs/id/:jobId')
+        .get(todoList.read_a_job)
+        .put(todoList.update_a_job)
+        .delete(todoList.delete_a_job);
+
+    // usage routes:
+    app.route('/usages')
+        .get(todoList.list_all_usages)
+        .post(todoList.create_a_usage);
+
+    app.route('/usages/id/:usageId')
+        .get(todoList.read_a_usage)
+        .put(todoList.update_a_usage)
+        .delete(todoList.delete_a_usage);
 };
