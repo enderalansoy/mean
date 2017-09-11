85fd4d880743ae789005a93b4ab1cbb8af38cf6d
enderalansoy <aendersoy@gmail.com>
2017-09-11 11:43:54 +0300

    Added a Procfile.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 8001,
+    port = process.env.PORT || 8002,
     mongoose = require('mongoose'),
     Task = require('./api/models/todoListModel'),
     bodyParser = require('body-parser');
+    
+app.use(require('forest-express-mongoose').init({
+    modelsDir: __dirname + '/api/models', // Your models directory.
+    envSecret: '59f08a6ba57a7d2d673ee5fefc483a5ef9075438da9283b73ce63a27bc991a75',
+    authSecret: 'wgKmHurjiIrOeAcahuwJCuhxnJh23vKe',
+    mongoose: require('mongoose') // The mongoose database connection.
+}));
 
 var uri = 'mongodb://aendersoy:iop890778@ds139985.mlab.com:39985/meanmongo';
 
